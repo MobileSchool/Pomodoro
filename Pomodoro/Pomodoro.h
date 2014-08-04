d07e9ef0b9a25d7095897c767c0fdd30272f9b8d
Minora, Leonardo <leonardo.minora@gmail.com>
2014-08-04 16:25:25 -0300

    Classe Pomodoro.h do model criada. Problemas: - Não foi especificado um package específico para a classe; - Propriedades estão com tipos errados.

---
+++ Pomodoro/Pomodoro.h
@@ -0,0 +1,18 @@
+//
+//  Pomodoro.h
+//  Pomodoro
+//
+//  Created by Leonardo Ataide Minora on 04/08/14.
+//  Copyright (c) 2014 Mobile School at IFRN-CNAT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pomodoro : NSObject
+
+@property NSInteger trabalho;
+@property NSInteger descanso;
+
+@property NSString *alarme;
+
+@end
