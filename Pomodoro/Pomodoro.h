b5eb4e46b46794c9d9f86929793339d181f6bfa6
Isso é um teste <fisicacomputacionalviii@new-host-19.home>
2014-08-06 16:00:25 -0300

    git config —global user.name : teste de publicação

--- Pomodoro/Pomodoro.h
+++ Pomodoro/Pomodoro.h
@@ -2,7 +2,7 @@
 //  Pomodoro.h
 //  Pomodoro
 //
-//  Created by Leonardo Ataide Minora on 04/08/14.
+//  Created by Leonardo Minora on 04/08/14.
 //  Copyright (c) 2014 Mobile School at IFRN-CNAT. All rights reserved.
 //
 
