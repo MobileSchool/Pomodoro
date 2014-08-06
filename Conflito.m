962c204c9cb984fa5579c84d4bf0236290838fea
Física Computacional VIII <fisicacomputacionalviii@new-host-19.home>
2014-08-06 14:57:29 -0300

    Git: Aprendendo a trabalhar com conflitos

---
+++ Conflito.m
@@ -0,0 +1,29 @@
+//
+//  Conflito.m
+//  Pomodoro
+//
+//  Created by Física Computacional VIII on 06/08/14.
+//  Copyright (c) 2014 Mobile School at IFRN-CNAT. All rights reserved.
+//
+
+#import "Conflito.h"
+
+@implementation Conflito
+
+-(void) dennis{
+    
+}
+
+-(void) italo{
+    
+}
+-(void) minora{
+    
+}
+-(void) julian{
+    
+}
+
+
+
+@end
