1c608970d3b7b7ce9ea1d3800fdcfb54160e26e6
Mobile School - Thiago <thiago@iMac-de-Fisica-6.local>
2014-08-18 12:57:35 -0300

    Aprendendo a commitar com o Xcode

--- Pomodoro/AppDelegate.m
+++ Pomodoro/AppDelegate.m
@@ -5,7 +5,7 @@
 //  Created by Mobile School - Julian on 30/07/14.
 //  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
 //
-
+// Comentario teste
 #import "AppDelegate.h"
 #import "Alarme.h"
 #import "Config.h"
