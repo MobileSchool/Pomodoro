a0224b53a0f954a0317369a8b42e11b45e1de3e3
Mobile School - Thiago <thiago@iMac-de-Fisica-6.local>
2014-08-18 13:06:09 -0300

    Removendo comentario desnecessario

--- Pomodoro/Pomodoro.h
+++ Pomodoro/Pomodoro.h
@@ -4,7 +4,7 @@
 //
 //  Created by Leonardo Minora on 04/08/14.
 //  Copyright (c) 2014 Mobile School at IFRN-CNAT. All rights reserved.
-// Comentario teste
+
 
 #import <Foundation/Foundation.h>
 #import "Time.h"
